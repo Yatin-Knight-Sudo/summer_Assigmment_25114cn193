7d570229b6855290b938845e329c5e1d9ed32408
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-05-28 11:18:13 +0530

    first commit

---
+++ Day1/2nd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,i,a=1;
+    printf("enter a number");
+    scanf("%d",&n);
+    for(i=1;i<=10;i++)
+    {
+        a=n*i;
+        printf("%d*%d=%d\n",n,i,a);
+    }
+
+    return 0;
+}
