6513271cfa864db97f081217ac48d3ba908a5337
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-05-30 17:32:29 +0530

    first commit

---
+++ Day_3/Q11.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    int p,q,i,a,b;
+    printf("input a number");
+    scanf("%d %d",&p,&q);
+    if(p>q)
+    {
+      a=q;
+    }
+    else 
+    {
+      a=p;  
+    }
+    for(i=1;i<=a;i++)
+    {
+        if(p%i==0 && q%i==0)
+        {
+             b=i;
+        }
+        
+    }
+    printf("%d", b);
+    
+    
+
+    return 0;
+}
