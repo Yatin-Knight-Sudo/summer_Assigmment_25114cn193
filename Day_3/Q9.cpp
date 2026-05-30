c250e5ecabb402c1ab36c7623fd60b220e0501f2
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-05-30 16:57:49 +0530

    Add Day 3 assignment files

---
+++ Day_3/Q9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+
+{
+    int n,c=0 ,i,d;
+    cout << "enter the number";
+    cin >>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        c++;
+        
+    }
+    if (c==2)
+    {
+        cout<<"the number is prime number";
+    }
+    else
+    {
+        cout<<"the number is not a prime number";
+    }
+
+}
