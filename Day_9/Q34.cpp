ff43c1c7d5f0fb5ea8f4d72f9e51a0891bc021f3
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-06-05 15:40:48 +0530

    first commit

---
+++ Day_9/Q34.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,l;
+    for(i=5;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+            cout<<j;;
+        }
+        cout<<endl;
+    }
+}
