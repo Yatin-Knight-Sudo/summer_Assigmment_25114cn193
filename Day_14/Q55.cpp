afb7202c2962737e8f4b13c0c26942d44ff38222
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-06-12 12:06:49 +0530

    first commit

--- Day_14/Q55.cpp
+++ Day_14/Q55.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 using namespace std;
 int main()
 {
@@ -11,21 +12,19 @@ int main()
         cout << "enter the element" << i+1 << endl;
         cin >> A[i];
     }
-    int p;int k=A[0];
+    int p;int k=INT_MIN;int f=INT_MIN;
     for(i=0;i<n;i++)
     {
         if(k<A[i])
         {
+            f=k;
             k=A[i];p=i;
-        }
-    }
-    A[p]=0;k=A[0];int c=0;
-    for(i=0;i<n;i++)
-    {
-        if(k<A[i])
+        }// if current element is smaller than largest but greatee than second largest
+        else if(f<A[i] && A[i]!=k)
         {
-            c=i;
+            f=A[i];
         }
     }
-    cout << "the second largest element is" << " " << A[c];
+   cout << "the second largest element is" << " " << f;
+   return 0;
 }
