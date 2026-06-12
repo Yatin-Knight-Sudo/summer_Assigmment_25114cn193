b84e92bf02e824e0ac3bc8c12ccaa3602e9275b9
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-06-12 15:32:39 +0530

    first commit

---
+++ Day_15/Q58.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+void revarr(int arr[],int f,int l)
+{
+    int i,j;
+    for(i=f,j=l;i<j;i++,j--)
+    {
+int temp=arr[i];
+arr[i]=arr[j];
+arr[j]=temp;
+    }
+}
+int main()
+{
+    int n;int i,j,k;
+    cout << "enter the size of aarray\n";
+    cin >> n;
+    int A[n];
+    for(i=0;i<n;i++)
+    {
+        cout <<" enter the element "  << i+1 << endl;
+        cin >> A[i];
+    }
+    cout << "enter by hom many time you want to rotate array by left\n";
+    cin >> k;
+    revarr(A,0,k-1);
+    revarr(A,k,n-1);
+    revarr(A,0,n-1);
+for(i=0;i<n;i++)
+{
+    cout << A[i] << " ";
+}
+
+}
