a9661f676d830366c6d26d73afe3bd62c9f3d131
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-06-18 19:30:16 +0530

    first commit

---
+++ Day_21/Q84.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cout << "Enter a string: ";
+    getline(cin, s);
+
+    for(int i = 0; i < s.size(); i++)
+    {
+        if(s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] = s[i] - 32;
+        }
+    }
+
+    cout << "Uppercase string: " << s;
+
+    return 0;
+}
