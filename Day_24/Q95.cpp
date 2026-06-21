f87678d50bdc0ada75f4acb4ce8b9270dbfe46a7
Yatin-Knight-Sudo <yatinpaliwal2025@gmail.com>
2026-06-21 16:20:34 +0530

    first commit

---
+++ Day_24/Q95.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s;
+
+    cout << "Enter a sentence: ";
+    getline(cin, s);
+
+    string word = "", longest = "";
+
+    for (int i = 0; i <= s.length(); i++)
+    {
+        if (i == s.length() || s[i] == ' ')
+        {
+            if (word.length() > longest.length())
+            {
+                longest = word;
+            }
+
+            word = "";
+        }
+        else
+        {
+            word = word + s[i];
+        }
+    }
+
+    cout << "Longest word: " << longest;
+
+    return 0;
+}
